ac42f58a2c6176fea80854679ddb3d8bdc2f271b
Mridul Roy <midlry.mr@gmail.com>
2023-09-09 04:49:48 +0530

    updates

--- Ubuntu/Install/Minikube.sh
+++ Ubuntu/Install/Minikube.sh
@@ -6,3 +6,6 @@ curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
 
 minikube start
+minikube kubectl -- get po -A
+echo 'alias kubectl="minikube kubectl --"' >> ~/.bashrc
+source ~/.bashrc
